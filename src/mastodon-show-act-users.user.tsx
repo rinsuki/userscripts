b440428d0c138c42ae797cbed53c0a7e8b75a5b3
rinsuki <428rinsuki+git@gmail.com>
2021-07-12 19:05:14 +0900

    ページ本体とは別空間で動くように

--- src/mastodon-show-act-users.user.tsx
+++ src/mastodon-show-act-users.user.tsx
@@ -7,7 +7,6 @@
 // @include       /^https:\/\/[^/]*\/@[A-Za-z0-9_]+\/[0-9]+([?#].*)?$/
 // @exclude-match https://*/@*/*/embed
 // @exclude-match https://*.tiktok.com/*
-// @grant         none
 // @require       https://cdn.jsdelivr.net/npm/react@16.13.1/umd/react.production.min.js#sha256=c9486f126615859fc61ac84840a02b2efc920d287a71d99d708c74b2947750fe
 // @require       https://cdn.jsdelivr.net/npm/react-dom@16.13.1/umd/react-dom.production.min.js#sha256=bc5b7797e8a595e365c1385b0d47683d3a85f3533c58d499659b771c48ec6d25
 // ==/UserScript==
