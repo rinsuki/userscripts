687b61c397b1b84b9cf937a6f190dd26a18c7277
rinsuki <428rinsuki+git@gmail.com>
2021-11-06 00:35:38 +0900

    niconico-embed-hq: remove unsafeWindow

--- simple-scripts/niconico-embed-hq.user.js
+++ simple-scripts/niconico-embed-hq.user.js
@@ -3,7 +3,6 @@
 // @namespace   https://rinsuki.net
 // @match       https://embed.nicovideo.jp/watch/*
 // @grant       GM_xmlhttpRequest
-// @grant       unsafeWindow
 // @connect     www.nicovideo.jp
 // @version     1.0
 // @author      -
@@ -12,28 +11,77 @@
 // @ts-check
 
 (() => {
-    // TODO: find more secure way, it is maybe insecure
-    const origFetch = unsafeWindow.fetch
-    unsafeWindow.fetch = (...args) => {
-        if (typeof args[0] === "string" && args[0].startsWith("https://www.nicovideo.jp/api/watch/v3_guest/")) {
+    /** @type {{error: string} | {url: string}} */
+    // @ts-expect-error
+    const IResponse = "you shouldn't use this as value"
+    function userland() {
+        const egui = document.getElementById("EMBED_HQ_USERSCRIPT")
+        const origFetch = window.fetch
+        window.fetch = (...args) => {
+            if (typeof args[0] !== "string") return origFetch(...args)
+            if (!args[0].startsWith("https://www.nicovideo.jp/api/watch/v3_guest/")) return origFetch(...args)
             console.log("hooking request", ...args)
+            const reqdiv = document.createElement("div")
+            reqdiv.dataset.url = args[0].replace("/v3_guest/", "/v3/").replace("?_frontendId=70&", "?_frontendId=6&") + "&withoutHistory=true"
             return new Promise((resolve, reject) => {
-                GM_xmlhttpRequest({
-                    method: "GET",
-                    url: JSON.parse(JSON.stringify(args[0])).replace("/v3_guest/", "/v3/").replace("?_frontendId=70&", "?_frontendId=6&") + "&withoutHistory=true",
-                    responseType: "blob",
-                    onload: resolve,
-                    onerror: r => reject(r.error)
+                const observer = new MutationObserver(events => {
+                    for (const event of events) {
+                        if (event.attributeName !== "data-response") continue
+                        /** @type {typeof IResponse} */
+                        const res = JSON.parse(reqdiv.dataset.response)
+                        console.log(res)
+                        if ("error" in res) {
+                            reject(res.error)
+                        } else {
+                            resolve(fetch(res.url).then(r => {
+                                URL.revokeObjectURL(res.url)
+                                return r
+                            }))
+                        }
+                        observer.disconnect()
+                    }
                 })
-            }).then((response) => {
-                console.log("!", response.response)
-                const url = URL.createObjectURL(response.response)
-                return unsafeWindow.fetch(url).then(r => {
-                    URL.revokeObjectURL(url)
-                    return r
+                observer.observe(reqdiv, {
+                    attributes: true,
+                    attributeFilter: ["data-response"]
                 })
+                egui.appendChild(reqdiv)
             })
         }
-        return origFetch(...args)
     }
+    const egui = document.createElement("div")
+    egui.id = "EMBED_HQ_USERSCRIPT"
+    egui.style.display = "none !important"
+    const script = document.createElement("script")
+    script.innerHTML = `(${userland.toString()})()`
+    egui.appendChild(script)
+    const observer = new MutationObserver(events => {
+        for (const event of events) {
+            for (const node of event.addedNodes) {
+                if (!(node instanceof HTMLDivElement)) continue
+                const url = new URL(node.dataset.url).href
+                if (!url.startsWith("https://www.nicovideo.jp/api/watch/v3/")) {
+                    return
+                }
+                console.log("hooking request received", url)
+                GM_xmlhttpRequest({
+                    method: "GET",
+                    url,
+                    responseType: "blob",
+                    onload(r) {
+                        console.log("!", r.response)
+                        const url = URL.createObjectURL(r.response)
+                        node.dataset.response = JSON.stringify({url})
+                    },
+                    onerror(r) {
+                        node.dataset.response = JSON.stringify({error: r.error})
+                    },
+                })
+            }
+        }
+    })
+    observer.observe(egui, {
+        childList: true,
+    })
+    document.body.appendChild(egui)
 })()
