f7bd1ef49d35e0f3b35690748e816906f557e300
rinsuki <428rinsuki+git@gmail.com>
2022-04-08 22:54:10 +0900

    fix

--- simple-scripts/youtube-embed-storage-access-api.user.js
+++ simple-scripts/youtube-embed-storage-access-api.user.js
@@ -9,33 +9,37 @@
 // @description Use Storage Access API to Play YouTube (Music)? Premium Required Videos on Embed Player
 // ==/UserScript==
 
-document.requestStorageAccess().then(()=> {
-    console.warn("[UserScript] --- requestStorageAccess success ---")
-}, () => {
-    console.warn("[UserScript] --- requestStorageAccess failed ---")
-})
-
-const origOpen = XMLHttpRequest.prototype.open
-const origSend = XMLHttpRequest.prototype.send
-const weakMap = new WeakMap()
-
-// addEventListener("mouseenter", () => document.requestStorageAccess())
-
-XMLHttpRequest.prototype.open = function(method, url, ...args) {
-    weakMap.set(this, url)
-    return origOpen.apply(this, [method, url, ...args])
-}
-
-XMLHttpRequest.prototype.send = function(...args) {
-    const url = weakMap.get(this)
-    if (!url.includes("youtubei/v1/player")) {
-        return origSend.apply(this, args)
-    }
+if ("requestStorageAccess" in document) {
     document.requestStorageAccess().then(()=> {
         console.warn("[UserScript] --- requestStorageAccess success ---")
     }, () => {
         console.warn("[UserScript] --- requestStorageAccess failed ---")
-    }).then(() => {
-        origSend.apply(this, args)
     })
+    
+    const origOpen = XMLHttpRequest.prototype.open
+    const origSend = XMLHttpRequest.prototype.send
+    const weakMap = new WeakMap()
+    
+    // addEventListener("mouseenter", () => document.requestStorageAccess())
+    
+    XMLHttpRequest.prototype.open = function(method, url, ...args) {
+        weakMap.set(this, url)
+        return origOpen.apply(this, [method, url, ...args])
+    }
+    
+    XMLHttpRequest.prototype.send = function(...args) {
+        const url = weakMap.get(this)
+        if (!url.includes("youtubei/v1/player")) {
+            return origSend.apply(this, args)
+        }
+        document.requestStorageAccess().then(()=> {
+            console.warn("[UserScript] --- requestStorageAccess success ---")
+        }, () => {
+            console.warn("[UserScript] --- requestStorageAccess failed ---")
+        }).then(() => {
+            origSend.apply(this, args)
+        })
+    }
+} else {
+    console.warn("[UserScript] -- requestStorageAccess not found --")
 }
