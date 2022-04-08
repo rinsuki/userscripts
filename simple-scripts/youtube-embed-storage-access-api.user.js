a72fcbbd91b3986a9db52e8964c66e77ef27c1f8
rinsuki <428rinsuki+git@gmail.com>
2022-04-08 22:43:33 +0900

    add youtube-embed-storage-access-api.user.js

---
+++ simple-scripts/youtube-embed-storage-access-api.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name        YouTube Embed: Use Storage Access API
+// @namespace   https://rinsuki.net
+// @match       https://www.youtube.com/embed/*
+// @grant       none
+// @version     1.0
+// @author      rinsuki
+// @license     MIT
+// @description Use Storage Access API to Play YouTube (Music)? Premium Required Videos
+// ==/UserScript==
+
+document.requestStorageAccess().then(()=> {
+    console.warn("[UserScript] --- requestStorageAccess success ---")
+}, () => {
+    console.warn("[UserScript] --- requestStorageAccess failed ---")
+})
+
+const origOpen = XMLHttpRequest.prototype.open
+const origSend = XMLHttpRequest.prototype.send
+const weakMap = new WeakMap()
+
+// addEventListener("mouseenter", () => document.requestStorageAccess())
+
+XMLHttpRequest.prototype.open = function(method, url, ...args) {
+    weakMap.set(this, url)
+    return origOpen.apply(this, [method, url, ...args])
+}
+
+XMLHttpRequest.prototype.send = function(...args) {
+    const url = weakMap.get(this)
+    if (!url.includes("youtubei/v1/player")) {
+        return origSend.apply(this, args)
+    }
+    document.requestStorageAccess().then(()=> {
+        console.warn("[UserScript] --- requestStorageAccess success ---")
+    }, () => {
+        console.warn("[UserScript] --- requestStorageAccess failed ---")
+    }).then(() => {
+        origSend.apply(this, args)
+    })
+}
