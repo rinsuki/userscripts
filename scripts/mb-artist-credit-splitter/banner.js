36ebc3524e83bcb8954ccf06bfd6d58e70f69a3b
rinsuki <428rinsuki+git@gmail.com>
2022-09-01 12:25:32 +0900

    mb-artist-credit-splliter: bump version

--- scripts/mb-artist-credit-splitter/banner.js
+++ scripts/mb-artist-credit-splitter/banner.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        MB: Artist Credit Splitter
-// @version     1.0.1
+// @version     1.0.2
 // @description いい感じに MusicBrainz のアーティストクレジットを分割します (失敗することもあります)
 // @namespace   https://rinsuki.net/
 // @author      rinsuki
