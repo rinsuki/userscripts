b352a489f1e95e331936598f0fee9d0c6492cf5b
rinsuki <428rinsuki+git@gmail.com>
2025-06-29 06:53:54 +0900

    update build dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,15 +2,15 @@ import fs from "fs"
 import pluginTypescript from "@rollup/plugin-typescript"
 import pluginNodeResolve from "@rollup/plugin-node-resolve"
 
-const files = fs.readdirSync(__dirname+"/scripts")
+const files = fs.readdirSync(import.meta.dirname+"/scripts")
 
 export default files.filter(a => !a.startsWith(".") && !a.endsWith("_common")).map(file => {
     return {
         input: "./scripts/" + file + "/src/index.tsx",
         output: [{
             name: file+".user",
-            file: __dirname+"/dist/" + file + ".user.js",
-            banner: () => fs.readFileSync(`${__dirname}/scripts/${file}/banner.js`, { encoding: "utf-8" }),
+            file: import.meta.dirname+"/dist/" + file + ".user.js",
+            banner: () => fs.readFileSync(`${import.meta.dirname}/scripts/${file}/banner.js`, { encoding: "utf-8" }),
             format: "iife",
         }],
         plugins: [
