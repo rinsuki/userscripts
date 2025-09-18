36edba1636c2856a390c05938a6b917a6bc54093
rinsuki <428rinsuki+git@gmail.com>
2025-09-18 18:33:06 +0900

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            nodejs_24
+            (pnpm.override {
+              nodejs = nodejs_24;
+            })
+          ];
+        };
+        formatter = pkgs.nixfmt-tree;
+      }
+    );
+}
+
