17f91aee4adb61d2a73d90c4d705e0180910e994
rinsuki <428rinsuki+git@gmail.com>
2025-10-11 17:34:00 +0900

    use our dom-chef fork for umd

--- external-globals.js
+++ external-globals.js
@@ -14,8 +14,8 @@ export const table = {
         var: "ReactDOM",
         path: "umd/react-dom.production.min.js",
     },
-    "dom-chef": {
+    "@rinsuki/dom-chef": {
         var: "DOMChef",
-        url: "https://update.greasyfork.org/scripts/545051/1637871/dom-chef%20511%20UMD%20version.js#sha256=12f18c54da1bda34617304a8f20186269de664910d35a430ae962535142550c7",
+        path: "umd.js",
     }
 }
